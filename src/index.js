196ec29bab96915a16867cb4e7c8d4a4ef238e06
martinproxy0 <marcinsornek@gmail.com>
2018-02-19 00:32:51 +0100

    Add DevTools

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
+import DevTools from './store/DevTools';
 
 ReactDOM.render(
-    <h1>Inicjalizacja projektu</h1>,
+    <Provider store={store}>
+        <div>
+            <h1>Inicjalizacja projektu</h1>
+            <DevTools />
+        </div>
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
+
