375fbded7318218ff38c6fa179fb502d0a3fcc57
martinproxy0 <marcinsornek@gmail.com>
2018-02-18 23:59:56 +0100

    Add initial code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <h1>Inicjalizacja projektu</h1>,
+    document.getElementById('root')
+);
+registerServiceWorker();
