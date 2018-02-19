021376f43ee7e965a36223d48415f38bc8300533
martinproxy0 <marcinsornek@gmail.com>
2018-02-19 23:48:57 +0100

    Add routes & bootstrap v4 & jquery

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
 import store from './store';
-import {getCountries} from './actions/actions-countries';
+import { getCountries } from './actions/actions-countries';
+import routes from './routes';
 import DevTools from './store/DevTools';
 
 ReactDOM.render(
     <Provider store={store}>
         <div>
-            <h1>Inicjalizacja projektu</h1>
+            <Router history={hashHistory} routes={routes}/>
             <DevTools />
         </div>
     </Provider>,
---
+++ src/presentational/navigation.component.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+//import 'bootstrap/dist/css/bootstrap-theme.css';
+
+
+const Navigation = props => (
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <Link class="navbar-brand" to="/">Państwa.js</Link>
+
+                    <ul className="navbar-nav">
+                        <li class="nav-item"><Link class="nav-link" to="/countries">Countries</Link></li>
+                        <li class="nav-item"><Link class="nav-link" to="/continents">Continents</Link></li>
+                        <li class="nav-item"><Link class="nav-link" to="/contact">Contact</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div className="container-fluid">
+            {props.children}
+        </div>
+    </div>
+);
+
+export default Navigation;
